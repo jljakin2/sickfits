459b398ca8114e3787c458001c53f433c45feb8a
jljakin2 <jeff.jakinovich@gmail.com>
2021-11-24 15:30:49 -0500

    delete product functionality in edit mode with automatic apollo cache eviction

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,41 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+// remove deleted item from apollo cache so the client side is updated right away without needing a refresh
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading, error }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: { id },
+      update,
+    }
+  );
+
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // go ahead and delete it
+          deleteProduct().catch((error) => alert(error.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
